0027500e4d20fe2f998b412f6e27042e62b182a5
Cato Skogholt <cskogholt@gmail.com>
2017-04-22 23:40:57 +0200

    Updated airports with new assets

--- game/scripts/Airport.gd
+++ game/scripts/Airport.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
 export(int) var max_capacity = 1
-export(int) var free_capacity = 1
+export(int) var free_capacity = 0
 
 onready var mgr = get_tree().get_root().find_node("FlightManager", true, false)
+onready var size1 = get_node("Size1")
+onready var size2 = get_node("Size2")
+onready var size3 = get_node("Size3")
 
 func _ready():
+	free_capacity = max_capacity
+	
+	if max_capacity > 0:
+		size1.show()
+	if max_capacity > 1:
+		size2.show()
+	if max_capacity > 2:
+		size3.show()
+
 	get_node("Label").set_text(get_name())
-	_update_label()
 	set_process(true)
 
 func _process(delta):
 	pass
-	
-func _update_label():
-	get_node("Capacity").set_text(str(free_capacity) + "/" + str(max_capacity))
 	
 func get_capacity():
 	return free_capacity
@@ -22,9 +30,10 @@ func get_capacity():
 func get_slot():
 	if free_capacity > 0:
 		free_capacity -= 1
-		_update_label()
+		update_capacity()
 		return true
 		
+	update_capacity()
 	return false
 	
 func free_slot():
@@ -32,7 +41,20 @@ func free_slot():
 	if free_capacity > max_capacity:
 		free_capacity = max_capacity
 		
-	_update_label()
+	update_capacity()
+
+func update_capacity():
+	size1.set_frame(0)
+	size2.set_frame(0)
+	size3.set_frame(0)
+	
+	var occupied = max_capacity - free_capacity
+	if occupied > 0:
+		size1.set_frame(1)
+	if occupied > 1:
+		size2.set_frame(1)
+	if occupied > 2:
+		size3.set_frame(1)
 
 func _on_MapSprite_clicked( pressed, offset ):
 	mgr.airport_click(self, pressed, offset)
